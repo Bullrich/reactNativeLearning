d7347117f5cd445ab837bc8079cfb08fa60fd2db
Javier Bullrich <javierbullrich@gmail.com>
2018-08-14 00:01:28 -0300

    Got to lecture 46

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Header headerText={'Albums'}/>
+        <AlbumList/>
       </View>
-    );
+    )
   }
 }
 
@@ -19,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Header = (props) => {
+  const {textStyle, viewStyle} = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 40
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    paddingTop: 25,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.4,
+    elevation: 2,
+    position: 'relative'
+  }
+});
+
+export default Header;
