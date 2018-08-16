f359c0d46c502dd1aaa66d0f8b2657e8459d7ee8
Javier Bullrich <jbullrich@jamcity.com>
2018-08-16 15:31:33 -0300

    Finished lesson 96

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native';
+import {FlatList} from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Hola!</Text>
-            </View>
-        );
-    }
+  renderItem(library) {
+    return <ListItem library={library}/>
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id.toString()}
+      />
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {libraries: state.libraries};
+  return {libraries: state.libraries};
 };
 
 export default connect(mapStateToProps)(LibraryList);
---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {CardSection} from "./common";
+import {StyleSheet, Text} from 'react-native';
+
+class ListItem extends Component {
+
+  render() {
+    const {titleStyle} = styles;
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.item.title}
+          </Text>
+      </CardSection>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  titleStyle:{
+    fontSize: 18,
+    paddingLeft: 15
+  }
+});
+
+
+export default ListItem;
