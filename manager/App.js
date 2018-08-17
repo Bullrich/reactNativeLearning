1ac8e7eb26149590a8daf96709f85899030e1893
Javier Bullrich <jbullrich@jamcity.com>
2018-08-17 15:26:18 -0300

    Finished lesson 108

--- manager/App.js
+++ manager/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './src/reducers'
+import firebase from 'firebase';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyBY4Yh70qyNyGvndwS6Q50tlOUnNvVy1qw",
+      authDomain: "manager-test-745d0.firebaseapp.com",
+      databaseURL: "https://manager-test-745d0.firebaseio.com",
+      projectId: "manager-test-745d0",
+      storageBucket: "manager-test-745d0.appspot.com",
+      messagingSenderId: "694805682973"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
     );
   }
 }
