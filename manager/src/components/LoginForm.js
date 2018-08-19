f6ec5cc8c12f1f8d98fcb46dc4a028dcb7702fa8
Javier Bullrich <javierbullrich@gmail.com>
2018-08-19 20:00:55 -0300

    Finished Lecture 137

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -14,9 +14,7 @@ class LoginForm extends Component {
   }
 
   onButtonPress() {
-    console.log(this.props);
     const {email, password} = this.props;
-    console.log(email + ' : ' + password);
     this.props.loginUser(email, password);
   }
 
